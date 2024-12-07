eaa1cbdc58e388863cd1889ca0a74ddb4b2f9a20
Thanasis Kyprianos <than20022004@gmail.com>
2024-12-07 20:27:18 +0200

    Add basic themes, add theme switching button on Header.

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,39 +1,21 @@
-import { Button } from '@mui/material';
-import { ThemeProvider, useColorScheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { BreadcrumbsWrapper as Breadcrumbs } from './components/BreadcrumbsWrapper';
 import { Header } from './components/Header';
 import { theme } from './config/theme.config';
 import { HomePage } from './pages/HomePage';
 
-const Switch = () => {
-  const { mode, setMode } = useColorScheme();
-  if (!mode) {
-    return <></>;
-  }
-
-  return (
-    <>
-      <Button
-        variant="outlined"
-        onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
-      >
-        {mode === 'dark' ? 'light' : 'dark'}
-      </Button>
-    </>
-  );
-};
-
 export const Routing = () => {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <BrowserRouter>
         <Header />
         <Breadcrumbs />
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
-        <Switch />
       </BrowserRouter>
     </ThemeProvider>
   );

899cae545e0b42a383d28bc14700343825a1f73e
Thanasis Kyprianos <than20022004@gmail.com>
2024-12-07 23:00:53 +0200

    Change breadcrumbs placement, remove breadcrumbs from home page.

--- src/components/BreadcrumbsWrapper.tsx
+++ src/components/BreadcrumbsWrapper.tsx
@@ -1,19 +1,27 @@
-import { Breadcrumbs, Link, Typography } from '@mui/material';
+import { Breadcrumbs, Link, SxProps, Theme, Typography } from '@mui/material';
 import { Link as RouterLink, LinkProps, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+
+interface BreadcrumbsProps {
+  sx: SxProps<Theme>;
+}
 
 const LinkRouter = (props: LinkProps) => {
   return <Link {...props} component={RouterLink} />;
 };
 
-export const BreadcrumbsWrapper = () => {
+export const BreadcrumbsWrapper = ({ sx }: BreadcrumbsProps) => {
   const { t } = useTranslation();
   const location = useLocation();
   const breadcrumbs: string[] = location.pathname.split('/').filter(x => x);
   console.log(breadcrumbs);
 
+  if (!breadcrumbs.length) {
+    return <></>;
+  }
+
   return (
-    <Breadcrumbs separator=">">
+    <Breadcrumbs separator=">" sx={sx}>
       {breadcrumbs.length ? (
         <LinkRouter to="/">
           <Typography variant="subtitle2">{t('breadcrumbs.home')}</Typography>

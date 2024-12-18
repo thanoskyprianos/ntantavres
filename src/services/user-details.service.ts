f60a3f25b76d843f47a7a40763c089b09c0f73bf
Thanasis Kyprianos <than20022004@gmail.com>
2024-12-18 23:54:52 +0200

    Set avatar limit to 1MB, add input badges.

--- src/services/user-details.service.ts
+++ src/services/user-details.service.ts
@@ -3,29 +3,13 @@ import { User } from 'firebase/auth';
 import { UserDetails } from '../types/UserDetails.ts';
 import { doc, setDoc } from 'firebase/firestore';
 import { Avatar, Base64String } from '../types/Avatar.ts';
-import i18n from 'i18next';
 
-const toBase64 = (file?: File): Promise<Base64String | void> => {
-  if (!file) {
+export const setUserAvatar = async (user: User, avatar?: Base64String) => {
+  if (!avatar) {
     return Promise.resolve();
   }
-
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result as string);
-    reader.onerror = error => reject(error);
-  });
-};
-
-export const setUserAvatar = async (user: User, avatar?: File) => {
-  // TODO: add 1mb size limit (after b64)
-
-  const b64 = await toBase64(avatar);
-  if (!b64) {
-    throw new Error(i18n.t('auth.avatarError'));
-  }
-  return await setDoc(doc(db, 'avatar', user.uid), { data: b64 } as Avatar);
+  
+  return await setDoc(doc(db, 'avatar', user.uid), { data: avatar } as Avatar);
 };
 
 export const setUserDetails = (user: User, details: UserDetails) => {
@@ -35,7 +19,7 @@ export const setUserDetails = (user: User, details: UserDetails) => {
 export const registerUser = (
   user: User,
   details: UserDetails,
-  avatar?: File
+  avatar?: Base64String
 ) => {
   return Promise.all([
     setUserDetails(user, details),

224d131131a1c5fdfdfad63e4433542155b4a19b
Thanasis Kyprianos <than20022004@gmail.com>
2025-01-03 16:16:19 +0200

    Commit missing files.

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,2 +1,4 @@
 export const removeEmptyFields = (obj: any) =>
   Object.fromEntries(Object.entries(obj).filter(([_, v]) => !!v));
+
+export const isEmpty = (obj: any) => Object.entries(obj).length === 0;

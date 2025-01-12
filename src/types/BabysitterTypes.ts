7b7be03d2fa54d0500954f000518153922457980
Thanasis Kyprianos <than20022004@gmail.com>
2025-01-12 15:11:30 +0200

    Fix some bugs, add gender selection.

--- src/types/BabysitterTypes.ts
+++ src/types/BabysitterTypes.ts
@@ -1,10 +1,13 @@
 import { Location } from '@/types/UserDetails.ts';
 import { WorkType } from '@/types/ParentAd.ts';
 
+export type Gender = 'male' | 'female' | 'other';
+
 export interface BabysitterTraits {
   experience?: string;
   studies?: string;
   about?: string;
+  gender?: Gender;
 }
 
 export interface BabysitterAd {

e3582be3aa8687118d2f9ed61cef79cbd5e5faaf
Thanasis Kyprianos <than20022004@gmail.com>
2025-01-12 20:16:38 +0200

    Add ability to schedule meetings

---
+++ src/types/Meeting.ts
@@ -0,0 +1,25 @@
+import { Location } from '@/types/UserDetails.ts';
+import { MonthAvailability } from '@/types/MonthAvailability.ts';
+
+export type Place = 'web' | 'in-person';
+
+export enum State {
+  PLANNED,
+  FINISHED,
+  CLOSED,
+}
+
+export interface Meeting {
+  place?: Place | null;
+  location?: Location | null;
+  dateTime?: Date | null;
+  interestedFor: keyof MonthAvailability | null;
+
+  uida?: string;
+  uidb?: string;
+  state?: State;
+
+  creation?: Date | null;
+
+  meetingId?: string;
+}

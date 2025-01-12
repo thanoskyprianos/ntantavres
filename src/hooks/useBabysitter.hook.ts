c01e2fb08f2c0c2e9c0308bbee15fb4cb2e132ef
Thanasis Kyprianos <than20022004@gmail.com>
2025-01-12 13:27:48 +0200

    Add actual babysitter ad, and rename other's to traits.

--- src/hooks/useBabysitter.hook.ts
+++ src/hooks/useBabysitter.hook.ts
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { MonthAvailability } from '@/types/MonthAvailability.ts';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { doc, getDoc, setDoc, updateDoc } from 'firebase/firestore';
 import { db } from '@config/firebase.ts';
-import { BabysitterAd } from '@/types/BabysitterAd.ts';
+import { BabysitterAd, BabysitterTraits } from '@/types/BabysitterTypes.ts';
 
 const _getAvailability = (uid: string) => {
   return getDoc(doc(db, 'availability', uid));
@@ -12,12 +12,24 @@ const _setAvailability = (uid: string, availability: MonthAvailability) => {
   return setDoc(doc(db, 'availability', uid), { ...availability });
 };
 
+const _getTraits = (uid: string) => {
+  return getDoc(doc(db, 'babysitter_traits', uid));
+};
+
+const _setTraits = (uid: string, ad: BabysitterTraits) => {
+  return setDoc(doc(db, 'babysitter_traits', uid), { ...ad });
+};
+
 const _getAd = (uid: string) => {
   return getDoc(doc(db, 'babysitter_ad', uid));
 };
 
 const _setAd = (uid: string, ad: BabysitterAd) => {
   return setDoc(doc(db, 'babysitter_ad', uid), { ...ad });
+};
+
+const _updateAd = (uid: string, ad: BabysitterAd) => {
+  return updateDoc(doc(db, 'babysitter_ad', uid), { ...ad });
 };
 
 export const useBabysitter = () => {
@@ -54,14 +66,41 @@ export const useBabysitter = () => {
     }
   };
 
+  const getTraits = async (uid: string) => {
+    setIsLoading(true);
+
+    let data: BabysitterTraits = {};
+    try {
+      data = (await _getTraits(uid)).data() as BabysitterTraits;
+    } catch {
+      data = {};
+    } finally {
+      setIsLoading(false);
+    }
+
+    return data;
+  };
+
+  const setTraits = async (uid: string, ad: BabysitterTraits) => {
+    setIsLoading(true);
+
+    try {
+      await _setTraits(uid, ad);
+    } catch {
+      throw new Error();
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   const getAd = async (uid: string) => {
     setIsLoading(true);
 
-    let data: BabysitterAd = {};
+    let data: BabysitterAd | undefined = undefined;
     try {
       data = (await _getAd(uid)).data() as BabysitterAd;
     } catch {
-      data = {};
+      data = undefined;
     } finally {
       setIsLoading(false);
     }
@@ -81,11 +120,27 @@ export const useBabysitter = () => {
     }
   };
 
+  const updateAd = async (uid: string, ad: BabysitterAd) => {
+    setIsLoading(true);
+
+    try {
+      await _updateAd(uid, ad);
+    } catch (err) {
+      console.log(err);
+      throw new Error();
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return {
     getAvailability,
     setAvailability,
+    getTraits,
+    setTraits,
     getAd,
     setAd,
+    updateAd,
     isLoading,
   };
 };

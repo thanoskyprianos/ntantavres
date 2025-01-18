446d467b2338e29e57e3180563d4379214b4f7d9
Thanasis Kyprianos <than20022004@gmail.com>
2025-01-19 01:09:21 +0200

    Remove old pages, re-add error boundary.

--- src/routing/Routing.tsx
+++ src/routing/Routing.tsx
@@ -3,17 +3,12 @@ import { Layout } from './Layout.tsx';
 import {
   AuthPage,
   BabysitterPage,
-  CardDetailPage,
   CollaborationPage,
   HomePage,
   ParentPage,
-  PaymentPage,
   ProfilePage,
   QuestionsPage,
   Ratings,
-  ScheduledMeetings,
-  TempRequestCompletion,
-  TempRequests,
 } from './lazy.routes.ts';
 import { ErrorPage } from '@pages/ErrorPage.tsx';
 
@@ -37,22 +32,6 @@ const router = createBrowserRouter([
             index: true,
             lazy: ParentPage,
           },
-          {
-            path: 'tempRequest',
-            lazy: TempRequests,
-          },
-          {
-            path: 'scheduledMeetings',
-            lazy: ScheduledMeetings,
-          },
-          {
-            path: 'tempRequestCompletion',
-            lazy: TempRequestCompletion,
-          },
-          {
-            path: 'paymentPage',
-            lazy: PaymentPage,
-          },
         ],
       },
       {
@@ -75,10 +54,6 @@ const router = createBrowserRouter([
       {
         path: '/ratings',
         lazy: Ratings,
-      },
-      {
-        path: '/interested',
-        lazy: CardDetailPage,
       },
       {
         path: '/profile/:uid',
